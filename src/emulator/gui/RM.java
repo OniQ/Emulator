3d86c9b6c51c33bbc291b5fca87ca1b010f299a0
OniQ <oniq.eg@gmail.com>
2014-03-22 15:57:51 +0200

    RM modifications

--- src/emulator/gui/RM.java
+++ src/emulator/gui/RM.java
@@ -45,6 +45,8 @@ public class RM extends javax.swing.JFrame {
         C = new javax.swing.JLabel();
         IC2 = new javax.swing.JTextField();
         CHST1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        listMemory = new javax.swing.JList();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -240,6 +242,15 @@ public class RM extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        listMemory.setBackground(new java.awt.Color(204, 255, 255));
+        listMemory.setFont(new java.awt.Font("Monospaced", 0, 11)); // NOI18N
+        listMemory.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        listMemory.setToolTipText("");
+        listMemory.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        listMemory.setSelectionBackground(new java.awt.Color(153, 255, 204));
+        listMemory.setSelectionForeground(new java.awt.Color(0, 0, 0));
+        jScrollPane1.setViewportView(listMemory);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -247,14 +258,20 @@ public class RM extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(178, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(37, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 26, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1))
+                .addContainerGap())
         );
 
         pack();
@@ -313,5 +330,7 @@ public class RM extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JList listMemory;
     // End of variables declaration//GEN-END:variables
 }
