eb0ca7b0d2c4eca5ac2a12417554c5479beaeb1f
OniQ <oniq.eg@gmail.com>
2014-03-30 21:31:27 +0300

    v1

--- src/emulator/vm/CommandInterpreter.java
+++ src/emulator/vm/CommandInterpreter.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package emulator.vm;
 
-/**
- *
- * @author Oni-Q
- */
 public class CommandInterpreter {
     
     VMController vmc;
@@ -24,25 +14,40 @@ public class CommandInterpreter {
         vmc.registers.setReg("IC", String.valueOf(newAdress));
     }
     
+    private String getMemoryPiece(String address){
+        String memory = Memory.getMemory(vmc.vm, address);
+        int counter = 0;
+        while(!memory.contains("$")){
+            address = Memory.next(address);
+            memory += Memory.getMemory(vmc.vm, address);
+            counter++;
+            if (counter > 10)
+                return null;
+        }
+        return memory;
+    }
+    
     public void execute(String command){
         Data.rmc.registers.setReg("SI", "0");
         Data.rmc.registers.setReg("PI", "0");
         Data.rmc.registers.setReg("CHST2", false);
+        Data.rmc.registers.setReg("CHST3", false);
         Data.rmc.registers.setIOI(2, false);
+        Data.rmc.registers.setIOI(3, false);
         if (command.length() != 4){
             System.err.println("Unsupported command: " + command);
         }
         else{
             String opcode = command.substring(0, 2);
-            String adress = command.substring(2, 4);
+            String address = command.substring(2, 4);
             switch(opcode){
                 case("PD"):
-                    vmc.vm.writeToConsole(Memory.getMemory(vmc.vm, adress));
+                    vmc.vm.writeToConsole(Memory.getMemory(vmc.vm, address));
                     Data.rmc.registers.setReg("SI", "2");
                     Data.rmc.registers.setReg("CHST2", true);
                     Data.rmc.registers.setIOI(2, true);
                     break;
-                case("GD"):
+                case("GD"):{
                     Data.rmc.registers.setReg("SI", "1");
                     Data.rmc.registers.setReg("CHST1", true);
                     Data.rmc.registers.setIOI(1, true);
@@ -50,45 +55,54 @@ public class CommandInterpreter {
                     Data.rmc.registers.setReg("CHST1", false);
                     Data.rmc.registers.setIOI(1, false);
                     vmc.vm.writeToConsole(": " + data + vmc.newLine);
-                    Memory.setMemory(vmc, adress, data);
+                    String word;
+                    while (data.length() > 4){
+                        word = data.substring(0, 4);
+                        data = data.substring(4);
+                        Memory.setMemory(vmc, address, word);
+                        address = Memory.next(address);
+                    }
+                    word = String.format("%-4s", data).replace(' ', '0');
+                    Memory.setMemory(vmc, address, word);
                     break;
+                }
                 case("LR"):
-                    vmc.registers.setReg("R", Memory.getMemory(vmc.vm, adress));
+                    vmc.registers.setReg("R", Memory.getMemory(vmc.vm, address));
                     break;
                 case("LD"):
-                    vmc.registers.setReg("D", Memory.getMemory(vmc.vm, adress));
+                    vmc.registers.setReg("D", Memory.getMemory(vmc.vm, address));
                     break;
                 case("SR"):
-                    Memory.setMemory(vmc, adress, vmc.registers.getR());
+                    Memory.setMemory(vmc, address, vmc.registers.getR());
                     break;
                 case("SD"):
-                    Memory.setMemory(vmc, adress, vmc.registers.getD());
+                    Memory.setMemory(vmc, address, vmc.registers.getD());
                     break;
                 case("JP"):
-                    vmc.registers.setReg("IC", adress);
+                    vmc.registers.setReg("IC", address);
                     break;
                 case("JT"):
                     if (vmc.registers.getC())
-                        vmc.registers.setReg("IC", adress);
+                        vmc.registers.setReg("IC", address);
                     break;
                 case("SU"):
                 {
                     int R = Integer.parseInt(vmc.registers.getR());
-                    R -= Integer.parseInt(Memory.getMemory(vmc.vm, adress));
+                    R -= Integer.parseInt(Memory.getMemory(vmc.vm, address));
                     vmc.registers.setReg("R", String.valueOf(R));
                     break;
                 }
                 case("AD"):
                 {
                     int R = Integer.parseInt(vmc.registers.getR());
-                    R += Integer.parseInt(Memory.getMemory(vmc.vm, adress));
+                    R += Integer.parseInt(Memory.getMemory(vmc.vm, address));
                     vmc.registers.setReg("R", String.valueOf(R));
                     break;
                 }
                 case("MU"):
                 {
                     int R = Integer.parseInt(vmc.registers.getR());
-                    R *= Integer.parseInt(Memory.getMemory(vmc.vm, adress));
+                    R *= Integer.parseInt(Memory.getMemory(vmc.vm, address));
                     if (R > 9999){
                         String value = String.format("%08d", R);
                         String hi = value.substring(0, 4);
@@ -105,15 +119,15 @@ public class CommandInterpreter {
                 case("DI"):
                 {
                     int R = Integer.parseInt(vmc.registers.getR());
-                    int D = R % Integer.parseInt(Memory.getMemory(vmc.vm, adress));
-                    R /= Integer.parseInt(Memory.getMemory(vmc.vm, adress));
+                    int D = R % Integer.parseInt(Memory.getMemory(vmc.vm, address));
+                    R /= Integer.parseInt(Memory.getMemory(vmc.vm, address));
                     vmc.registers.setReg("D", String.valueOf(D));
                     vmc.registers.setReg("R", String.valueOf(R));
                     break;
                 }
                 case("CH"):{
                     int R = Integer.parseInt(vmc.registers.getR());
-                    if (R > Integer.parseInt(Memory.getMemory(vmc.vm, adress))){
+                    if (R > Integer.parseInt(Memory.getMemory(vmc.vm, address))){
                         vmc.registers.setReg("C", true);
                     }
                     else{
@@ -123,12 +137,72 @@ public class CommandInterpreter {
                 }
                 case("CE"):{
                     int R = Integer.parseInt(vmc.registers.getR());
-                    if (R == Integer.parseInt(Memory.getMemory(vmc.vm, adress))){
+                    if (R == Integer.parseInt(Memory.getMemory(vmc.vm, address))){
                         vmc.registers.setReg("C", true);
                     }
                     else{
                         vmc.registers.setReg("C", false);
                     }
+                    break;
+                }
+                case("FO"):{
+                    Data.rmc.registers.setReg("CHST3", true);
+                    Data.rmc.registers.setIOI(2, false);
+                    int descriptor = vmc.data.files.size();
+                    String fName = getMemoryPiece(address);
+                    fName = fName.substring(0, fName.indexOf("$", fName.length() - 4));
+                    if (fName != null){
+                        vmc.data.registerFile(fName);
+                        vmc.registers.setReg("D", String.valueOf(descriptor));
+                    }
+                    break;
+                }
+                case("FW"):{
+                    Data.rmc.registers.setReg("CHST3", true);
+                    Data.rmc.registers.setIOI(2, false);
+                    String memory = getMemoryPiece(address);
+                    memory = memory.substring(0, memory.indexOf("$", memory.length() - 4));
+                    int descriptor = Integer.parseInt(vmc.registers.getD());
+                    vmc.data.writeFile(descriptor, memory);
+                    break;
+                }
+                case("FR"):{
+                    Data.rmc.registers.setReg("CHST3", true);
+                    Data.rmc.registers.setIOI(2, false);
+                    int descriptor = Integer.parseInt(vmc.registers.getD());
+                    String fileData = vmc.data.readFile(descriptor);
+                    String word;
+                    while (fileData.length() > 4){
+                        word = fileData.substring(0, 4);
+                        fileData = fileData.substring(4);
+                        Memory.setMemory(vmc, address, word);
+                        address = Memory.next(address);
+                    }
+                    word = String.format("%-4s", fileData).replace(' ', '0');
+                    Memory.setMemory(vmc, address, word);
+                    break;
+                }
+                case("FD"):{
+                    Data.rmc.registers.setReg("CHST3", true);
+                    Data.rmc.registers.setIOI(2, false);
+                    int descriptor = Integer.valueOf(vmc.registers.getD());
+                    vmc.data.files.get(descriptor).delete();
+                    vmc.data.files.remove(descriptor);
+                    break;
+                }
+                case("FC"):{
+                    Data.rmc.registers.setReg("CHST3", true);
+                    Data.rmc.registers.setIOI(2, false);
+                    /*int descriptor = Integer.valueOf(vmc.registers.getD());
+                    try{
+                        vmc.data.in.get(descriptor).close();
+                        vmc.data.out.get(descriptor).close();
+                    }
+                    catch(IndexOutOfBoundsException  ex){
+                        System.err.println("Wrong descriptor");
+                    } catch (IOException ex) {
+                        System.err.println(ex);
+                    }*/
                     break;
                 }
                 case("HA"):
